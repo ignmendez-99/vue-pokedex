1d3c1261e5c3246b237ade5f545e0e124c593dab
Ignacio Mendez <ignmendez@itba.edu.ar>
2020-08-03 17:06:45 -0300

    PokemonList now loads pokemons 50 by 50 (name + photo). Axios added. I missed these changes

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,31 +1,43 @@
+<script>/* eslint-disable prettier/prettier */</script>
+
 <template>
   <v-container fluid>
     <v-row>
       <v-card>
         <v-card-title>Pokémon List</v-card-title>
-        <v-row justify="center" align="center" v-if="this.loadingPokemons">
+        <v-row
+          justify="center"
+          align="center"
+          v-if="this.loadingInitialPokemons"
+        >
           <div class="ma-12">
             <v-progress-circular
-              class="loading-circle"
               indeterminate
               color="red"
             ></v-progress-circular>
           </div>
         </v-row>
-        <v-list v-else avatar rounded>
-          <v-list-item-group v-model="pokemons.length" color="primary">
-            <v-list-item v-for="(pokemon, i) in pokemons" :key="i">
-              <!-- <v-list-item-avatar>
-                <v-img :src="pokemon.image"></v-img>
-              </v-list-item-avatar> -->
+        <v-list v-else avatar rounded width="270">
+          <v-list-item-group v-model="displayedPokemons.length" color="primary">
+            <v-list-item v-for="(pokemon, i) in displayedPokemons" :key="i">
+              <v-list-item-avatar>
+                <v-img :src="pokemon.imageURL"></v-img>
+              </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title v-html="pokemon.name"></v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
         </v-list>
+        <v-row justify="center" align="center">
+          <v-btn
+            class="mb-5"
+            :loading="this.loadingMorePokemons"
+            v-show="!this.loadingInitialPokemons"
+            @click="loadMorePokemons()"
+          >SHOW MORE</v-btn>
+        </v-row>
       </v-card>
-      <v-btn @click="getAllPokemons">HOLA</v-btn>
     </v-row>
   </v-container>
 </template>
@@ -33,33 +45,78 @@
 <script>
 const pokedex = require("pokeapi-js-wrapper");
 const P = new pokedex.Pokedex();
+const axios = require("axios");
 export default {
   name: "PokemonList",
   data: () => {
     return {
-      pokemons: [],
-      loadingPokemons: true
+      initialLoadingPokemons: 50,
+      numberOfDisplayedPokemons: 50,
+      displayedPokemons: [],
+      pokemonsNotSorted: [],
+      loadingInitialPokemons: true,
+      loadingMorePokemons: false,
+
+      // huge data
+      allPokemonsDataURL: []
     };
   },
   methods: {
-    getAllPokemons() {
-      let pokemonsCopy = this.pokemons;
-      let thisVue = this;
-      P.getPokemonsList().then(function(response) {
-        response.results.forEach(pokemon => {
-          pokemonsCopy.push({
-            name: pokemon.name,
-            url: pokemon.url,
-            photo: {}
+    loadMorePokemons() {
+      this.loadingMorePokemons = true;
+      const currentMax = this.numberOfDisplayedPokemons;
+      for (let index = currentMax; index < currentMax + 50; index++) {
+        axios.get(this.allPokemonsDataURL[index]).then(response => {
+          this.pokemonsNotSorted.push({
+            id: response.data.id,
+            name: response.data.name,
+            imageURL: response.data.sprites.front_default
           });
-          thisVue.getPokemonPhoto(pokemon.url);
+        });   
+      }
+
+      this.numberOfDisplayedPokemons = currentMax + 50;
+
+      setTimeout(() => {
+        this.pokemonsNotSorted.sort((pokemon1, pokemon2) => {
+          return pokemon1.id - pokemon2.id;
         });
-        thisVue.loadingPokemons = false;
-      });
-    },
-    getPokemonPhoto(url) {
-      console.log(url);
+        this.displayedPokemons = this.pokemonsNotSorted.slice();
+        this.loadingMorePokemons = false;
+      }, 3000);
     }
+  },
+  mounted() {
+    let thisVue = this;
+    let counter = 0;
+    P.getPokemonsList().then(response => {
+      
+      response.results.forEach(pokemon => {
+        const pokemonDataURL = pokemon.url;
+        thisVue.allPokemonsDataURL.push(pokemonDataURL);
+
+        if (counter < thisVue.initialLoadingPokemons) {
+
+          axios.get(pokemonDataURL).then(response => {
+            thisVue.pokemonsNotSorted.push({
+              id: response.data.id,
+              name: response.data.name,
+              imageURL: response.data.sprites.front_default
+            });
+          });
+
+          counter++;
+        }
+      });
+
+      setTimeout(() => {
+        thisVue.pokemonsNotSorted.sort((pokemon1, pokemon2) => {
+          return pokemon1.id - pokemon2.id;
+        });
+        thisVue.displayedPokemons = thisVue.pokemonsNotSorted.slice();
+        thisVue.loadingInitialPokemons = false;
+      }, 3000);
+    });
   }
 };
 </script>
